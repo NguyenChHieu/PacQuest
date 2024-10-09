a12e5de417b6b0bdeb2153682c67765c669ba186
Hieu Nguyen <cngu0360@uni.sydney.edu.au>
2024-10-09 22:17:13 +1100

    add RenderableType.java SuperPellet

--- home/src/main/java/pacman/model/factories/RenderableType.java
+++ home/src/main/java/pacman/model/factories/RenderableType.java
@@ -11,6 +11,7 @@ public interface RenderableType {
     char DOWN_LEFT_WALL = '5';
     char DOWN_RIGHT_WALL = '6';
     char PELLET = '7';
+    char SUPER_PELLET = 'z';
     char PACMAN = 'p';
     char BLINKY = 'b';
     char INKY = 'i';
